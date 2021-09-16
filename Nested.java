2b91124e99c031a78460125e25f3d51cb9119950
SairamNomula <sairamnomula14@gmail.com>
2021-09-16 20:18:16 +0530

    commit4

---
+++ Nested.java
@@ -0,0 +1,11 @@
+public class Nested {
+    public static void main(String[]args){
+        int n = 10;
+        if ( n > 10 )
+            System.out.println(">10");
+        else if( n == 10 )
+                System.out.println("=10");
+        else if ( n < 10 )
+                System.out.println("<10");
+    }
+}
