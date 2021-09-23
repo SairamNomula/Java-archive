103a70ce20f6c1d7fc164cabe5852d8aef3505eb
SairamNomula <sairamnomula14@gmail.com>
2021-09-23 11:09:10 +0530

    commit10

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table {
+    public static void main(String[]args){
+        for (int i = 1; i <= 10; i++);{
+        for (int j = 1; j <=10; j++);
+        System.out.print((i*j) + " ");
+        System.out.println();
+        }
+    }
+}
+
+// The name of the variable in the outer loop must be different than the inner loop.
