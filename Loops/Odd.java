6890c145e352cb74118683f43bda8b8dfb7ac4cb
SairamNomula <sairamnomula14@gmail.com>
2021-09-25 18:33:35 +0530

    Loop4

---
+++ Loops/Odd.java
@@ -0,0 +1,19 @@
+public class Odd {
+    public static void main(String[]args){
+        for(int i = 99; i>=1; i-=2)
+        System.out.println(i + " ");
+    }
+}
+
+// Method 2
+// public class Odd{
+//     public static void main(String[]args){
+//         int i = 100;
+//         while(i>=1){
+//             if(i%2==0)
+//             System.out.print(i + " ");
+
+//             i--;
+//         }
+//     }
+// }
