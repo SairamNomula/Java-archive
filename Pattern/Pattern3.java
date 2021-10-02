695737ad5ba1e52119b599a6e300e610b9e30827
SairamNomula <sairamnomula14@gmail.com>
2021-10-02 19:34:05 +0530

    pattern3

---
+++ Pattern/Pattern3.java
@@ -0,0 +1,21 @@
+// Write a program that reads a positive integer N and displays the following:
+// N = 4:
+//     *
+//    * *
+//   *   *
+//  *******
+
+import java.util.Scanner;
+public class Pattern3 {
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+            try{
+                
+            }
+            finally{
+                s.close();
+        }
+    }
+}
