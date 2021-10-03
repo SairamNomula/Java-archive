800bbe4a9ed9b7df8611fdb3b5d4f7620a0259a3
SairamNomula <sairamnomula14@gmail.com>
2021-10-03 14:12:46 +0530

    pattern3

--- Pattern/Pattern3.java
+++ Pattern/Pattern3.java
@@ -12,7 +12,24 @@ public class Pattern3 {
 
         int n = s.nextInt();
             try{
-                
+                for(int i =1; i<=n; i++){
+
+                    for(int j =1; j<=n-i; j++)
+                    System.out.print("");
+
+                    for(int j=1; j<=2*i-1; j++)
+
+                    if(i == n)
+                    System.out.print("*");
+
+                    else 
+                        if(j==1 || j==2*i-1)
+                        System.out.print("*");
+                        
+                    else
+                        System.out.println(" ");
+
+                }
             }
             finally{
                 s.close();
