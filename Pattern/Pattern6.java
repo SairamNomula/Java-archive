21af5f4c90f17951febc3335b11fdc228a4eacbf
SairamNomula <sairamnomula14@gmail.com>
2021-10-15 11:57:29 +0530

    Pattern6

---
+++ Pattern/Pattern6.java
@@ -0,0 +1,36 @@
+// N = 3    N = 5
+// ***      *****
+//  *        ***
+// ***       ***
+//           ***
+//          *****
+
+import java.util.Scanner;
+public class Pattern6 {
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        try{
+            for (int i = 1; i <= n; i++){
+                if(i == 1 || i == n)
+                
+            for (int j = 1; j<= n; j++)
+                    System.out.print("*");
+
+            else
+                for(int j =1; j<= n; j++)
+                    if(j == 1 || j ==n)
+                        System.out.print(" ");
+                    else
+                        System.out.print("*");
+
+            System.out.println();
+            }
+        }
+
+        finally{
+            s.close();
+        }
+    }
+}
