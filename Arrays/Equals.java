217b7bccf584e93f9482f122f7acb7931eb14ad4
SairamNomula <sairamnomula14@gmail.com>
2021-11-23 15:12:24 +0530

    Arrays

--- Arrays/Equals.java
+++ Arrays/Equals.java
@@ -3,3 +3,15 @@
 
 // System.out.println( str1 == str2 ); //false
 // System.out.println( str1.equals(str2) ); //true
+
+// -------------------------------------------------------------
+
+// Point[] points1 = {new Point(1,2), new Point(3,4)};
+// Point[] points2 = {new Point(1,2), new Point(3,4)};
+// Point[] points3 = {new Point(0,2), new Point(3,4)};
+
+// System.out.println(points1 == points2); //false
+// System.out.println(Arrays.equals(points1, points2)); //true
+// System.out.println(Arrays.equals(points1, points3)); //false
+
+// ------------------------------------------------------------
