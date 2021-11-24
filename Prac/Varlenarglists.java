db0f78afe619db2bb989c18c855d1e9fefb633f0
SairamNomula <sairamnomula14@gmail.com>
2021-11-24 10:07:27 +0530

    commit

---
+++ Prac/Varlenarglists.java
@@ -0,0 +1,19 @@
+// public class Varlenarglists {
+//     public static void main(String[] args){
+//         System.out.println( sum(1,7)); // 8
+//         System.out.println( sum(1,9,-1)); // 9
+//         System.out.println( sum(1,3,-3,1)); // 2
+//         System.out.println( sum(0,1,99,-1,2)); // 101
+//     }
+// }
+
+// -------------------------------------------------------------
+
+// public static int sum(int... numbers){
+//     int sum =0;
+//     for(int i =0; i< numbers.length; i++)
+//     sum += numbers[i];
+
+//     return sum;
+// }
+
