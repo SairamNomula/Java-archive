b352d6301367b5f3a4285dcbbe3e039adda704a9
SairamNomula <sairamnomula14@gmail.com>
2021-11-24 10:06:41 +0530

    Arrays

---
+++ Arrays/FillandPrint.java
@@ -0,0 +1,11 @@
+// fill(array, value): fill whole array
+// int[] numbers1 = new int[8]; // {0,0,0,0,0,0,0,0}
+// Arrays.fill(numbers1, 3); // {3,3,3,3,3,3,3,3}
+
+// fill(array, fromIndex, toIndex, value)
+// int[] numbers2 = new int[8]; // {0,0,0,0,0,0,0,0}
+// Arrays.fill(numbers2, 3, 7, 5); // {0,0,0,5,5,5,5,0}
+
+// -------------------------------------------------------------
+// System.out.println(Arrays.toString(strings)); // [hello,hello,hello]
+// System.out.println(Arrays.toString(points)); // [java.awt.Point[x=1,y=2], java.awt.Point[x=1,y=2], null]
