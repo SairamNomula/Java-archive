c7e5df0412237d5547cf99ae0f99a1c66a0b308e
SairamNomula <sairamnomula14@gmail.com>
2022-02-28 16:25:48 +0530

    Palindrome

---
+++ Programs/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Palindrome {
+    public static void main(String[]args){
+        String original, reverse = ""; //Objects of string class
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        original = in.nextLine();
+        
+        int length = original.length();
+
+        for(int i = length - 1;i>=0;i--)
+        reverse = reverse + original.charAt(i);
+
+        if(original.equals(reverse))
+        System.out.println("Palindrome");
+        else
+        System.out.println("Not Palindrome");
+    }
+}
