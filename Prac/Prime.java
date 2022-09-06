4a4cbe47fa471d4fbccf48d01e62037be3a9b5f9
SairamNomula <sairamnomula14@gmail.com>
2022-09-07 00:08:50 +0530

    Prime

--- Prac/Prime.java
+++ Prac/Prime.java
@@ -4,6 +4,7 @@ class Prime{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
+        sc.close();
         int number = sc.nextInt();
         if (isPrime(number)){
             System.out.println(number + " is a Prime");
